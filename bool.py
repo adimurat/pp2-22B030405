c39d9f27ea92a7d8c2cca8f82b0df401eaf1b990
adimurat <adlet.murat.04@bk.ru>
2023-01-24 20:17:33 +0300

    9 commit

---
+++ bool.py
@@ -0,0 +1,11 @@
+#When we run a condition in an if statement,pythin returns True or False:
+print(10 > 9) 
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
