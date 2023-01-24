595ba3e3ae4fa59c9f8024e8b4057748ae72f7af
adimurat <adlet.murat.04@bk.ru>
2023-01-24 20:23:06 +0300

    10 commit

---
+++ operators.py
@@ -0,0 +1,7 @@
+print(10 + 5) # addition
+print(10 - 5) # subtraction
+print(10 * 5) # multiplication
+print(10 / 5) # division
+print(10 % 5) # modulus
+print(10 ** 5) # exponentiation
+print(10 // 5) #floor division
