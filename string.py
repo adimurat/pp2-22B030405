72cd31fb57330a0640c1ddccc9c869b07ffcbcda
adimurat <adlet.murat.04@bk.ru>
2023-01-24 20:05:42 +0300

    8 commit

---
+++ string.py
@@ -0,0 +1,16 @@
+print("Hello")
+print('Hello') # it is same things
+a = "Hello"
+print(a)
+
+# strings are array
+a = "Hello,World!"
+print(a[1])
+
+#looping in string
+for x in "banana":
+    print(x)
+
+#string length
+a = "Hello world"
+print(len(a))
