b0f7e27d63b3da7212a9f9c39a227a6cbb6892a8
adimurat <adlet.murat.04@bk.ru>
2023-01-24 19:43:12 +0300

    5 commit

---
+++ Datatypes.py
@@ -0,0 +1,13 @@
+x = 5 # type int
+print(type(x))
+x = "John" # type str
+x = 20.5 # type float
+x = 1j # type complex
+x = ["apple", "banana", "cherry"] # list
+x = ("apple", "banana", "cherry") # tuple
+x = range(6) # range
+x = {"name" : "John", "age" : 36} # dict
+x = {"aplle", "banana", "cherry"} # set
+x = True # bool
+x = b"Hello" # bytes
+x = None # nonetype
