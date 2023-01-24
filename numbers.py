0b7ee06460bb30c19ade897cbb3c7f7a4610d7ba
adimurat <adlet.murat.04@bk.ru>
2023-01-24 19:52:56 +0300

    6 commit

---
+++ numbers.py
@@ -0,0 +1,20 @@
+x = 1 # int
+x = 2.8 # float
+x = 1j # complex
+
+#convert from int to float
+a = float(x)
+
+#convert from float to int
+b = int(x)
+
+#convert from int tp complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+#random numbers
+import random
+print(random.randrange(1,10))
